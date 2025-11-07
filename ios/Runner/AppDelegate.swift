bbadce3864caf8b30e27c49946f22a51baa1914f
hyeonggeun <polkmn0721@gmail.com>
2025-11-08 01:48:46 +0900

    feat: 모든 기능 구현 및 버그 수정 완료 (지도/필터/상세페이지)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps     // 1. Google Maps import
+import FirebaseCore   // 2. Firebase Core import
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,13 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // 3. Firebase 초기화 (Crashlytics 등에 필요)
+    FirebaseApp.configure()
+
+    // 4. Google Maps API 키 설정
+    GMSServices.provideAPIKey("AIzaSyDDEMu6gdsvgl6srD-kDGrUTVtVwJ28hmM")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
